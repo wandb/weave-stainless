4c16d40a691a22dcfccd57265f33e2eb0e43c0c0
Samuel El-Borai <sam@elborai.me>
2025-01-22 13:27:06 +0100

    Fix import

--- src/weave_trace/types/shared/__init__.py
+++ src/weave_trace/types/shared/__init__.py
@@ -1,7 +1,7 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 # import last
-from wand_demo._compat import PYDANTIC_V2
+from weave_trace._compat import PYDANTIC_V2
 
 from .expr import Expr as Expr
 from .operation import Operation as Operation
